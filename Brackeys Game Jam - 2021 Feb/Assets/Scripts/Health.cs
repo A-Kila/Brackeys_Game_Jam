6ecdac181876038722d66c64c1b1558c274d862f
A-Kila <akila19@freeuni.edu.ge>
2021-02-14 20:55:17 +0400

    Add health
    added health
    updated movement
    updated manager for health
    added collider to the projectile

---
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Health {
+	
+	public System.Action onPlayerDeath;
+
+	private int maxHealth;
+	public int health;
+
+	public Health(int health) {
+		maxHealth = health;
+		this.health = maxHealth;
+    }
+
+	public void DamageHealth(int amount) {
+		health -= amount;
+		health = Mathf.Clamp(health, 0, maxHealth);
+
+		if (health <= 0 && onPlayerDeath != null)
+			onPlayerDeath();
+	}
+
+	public void AddHealth(int amount) {
+		health += amount;
+		health = Mathf.Clamp(health, 0, maxHealth);
+    }
+
+}
