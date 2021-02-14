6a8478faff05c3ff44370104c7db81b15452752f
A-Kila <akila19@freeuni.edu.ge>
2021-02-14 18:55:38 +0400

    Updated Generalized Movement

---
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/CellManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CellManager : MonoBehaviour {
+	
+    public float speed = 15f;
+
+    private CellMovement movement;
+    private Camera gameCamera;
+
+    void Start() {
+        gameCamera = FindObjectOfType<Camera>();
+        movement = FindObjectOfType<CellMovement>();
+
+        movement.speed = speed;
+    }
+
+    void Update() {
+        MoveCell();
+    }
+
+    private void MoveCell() {
+        if (Input.GetMouseButtonDown(0)) { 
+            Vector2 posOnWorldMap = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+            movement.MoveLocation(posOnWorldMap);
+        }
+    }
+}
