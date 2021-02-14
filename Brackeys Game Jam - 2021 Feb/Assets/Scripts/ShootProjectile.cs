e37279c6e6720ecf45eb9fb3d30bd5bb7c3a8449
Nargizi <77239668+Nargizi@users.noreply.github.com>
2021-02-14 19:52:35 +0400

    Added Shooting mechanics

---
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Projectile;
+    private Vector3 TargetLoc;
+    private bool shoot = false;
+    // Start is called before the first frame update
+    public void setTarget(Vector3 targ)
+    {
+        TargetLoc = targ;
+        shoot = true;
+    }
+
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shoot)
+        {
+            creatProjectile();
+            shoot = false;
+        }
+    }
+
+    void creatProjectile()
+    {
+        Transform projTransform = Instantiate(Projectile, transform.position, Quaternion.identity);
+        projTransform.GetComponent<MoveProjectile>().setup(aimDirection());
+    }
+
+    Vector3 aimDirection()
+    {
+        Vector3 dir = (TargetLoc - transform.position).normalized;
+        return dir;
+    }
+
+}
