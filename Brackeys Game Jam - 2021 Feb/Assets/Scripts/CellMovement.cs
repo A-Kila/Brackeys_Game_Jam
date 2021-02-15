3186176f75e6b581198f81576c047d9dd96fca18
A-Kila <akila19@freeuni.edu.ge>
2021-02-15 16:19:41 +0400

    fixed bug

--- Brackeys Game Jam - 2021 Feb/Assets/Scripts/CellMovement.cs
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/CellMovement.cs
@@ -6,18 +6,18 @@ public class CellMovement : MonoBehaviour {
     private Rigidbody2D rb;
 
     private Vector2 moveTowards;
+    private Vector2 nextPosition;
     private float halfWidth, halfHeight, halfRBRadius;
 
     void Start() {
         rb = GetComponent<Rigidbody2D>();
         moveTowards = transform.position;
+        nextPosition = moveTowards;
 
         halfHeight = Camera.main.orthographicSize;
         halfWidth = halfHeight * Camera.main.aspect;
         halfRBRadius = rb.transform.localScale.x / 2;   
     }
-
-    private Vector2 nextPosition;
 
     void Update() {
         nextPosition = Vector2.MoveTowards(rb.position, moveTowards, speed * Time.deltaTime);
@@ -29,8 +29,10 @@ public class CellMovement : MonoBehaviour {
     }
 
     public void MoveLocation(Vector2 moveLocation) {
-        moveLocation.x = Mathf.Clamp(moveLocation.x, -halfWidth + halfRBRadius, halfWidth - halfRBRadius);
-        moveLocation.y = Mathf.Clamp(moveLocation.y, -halfHeight + halfRBRadius, halfHeight - halfRBRadius);
+        if (!(rb.isKinematic)) { 
+            moveLocation.x = Mathf.Clamp(moveLocation.x, -halfWidth + halfRBRadius, halfWidth - halfRBRadius);
+            moveLocation.y = Mathf.Clamp(moveLocation.y, -halfHeight + halfRBRadius, halfHeight - halfRBRadius);
+        }
         moveTowards = moveLocation;
     }
 
