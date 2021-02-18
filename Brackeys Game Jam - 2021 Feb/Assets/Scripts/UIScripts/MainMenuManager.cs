a0161edecf5e544245552519c836495156611856
A-Kila <akila19@freeuni.edu.ge>
2021-02-18 17:56:52 +0400

    basic main menu functionality

---
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/UIScripts/MainMenuManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject levelSelector;
+    public GameObject options;
+    public GameObject help;
+
+    private GameObject activeOverlay;
+
+    void Start() {
+        activeOverlay = mainMenu;
+    }
+
+    public void Play() {
+        activeOverlay.SetActive(false);
+        levelSelector.SetActive(true);
+    }
+
+    public void Options() {
+        activeOverlay.SetActive(false);
+        options.SetActive(true);
+    }
+
+    public void Help() {
+        activeOverlay.SetActive(false);
+        help.SetActive(true);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+
+}
