6de4e819a32dc3ef9e706295945eeb5ec98cceb5
A-Kila <akila19@freeuni.edu.ge>
2021-02-18 19:43:41 +0400

    commit this

--- Brackeys Game Jam - 2021 Feb/Assets/Scripts/UIScripts/MainMenuManager.cs
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/UIScripts/MainMenuManager.cs
@@ -1,36 +1,54 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
 
+    /* Main menu windows */
     public GameObject mainMenu;
     public GameObject levelSelector;
     public GameObject options;
     public GameObject help;
 
+    /* Help window varibles */
+    public Vector2 helpPanelAlpha = new Vector2(40f, 230f);
+    public Image panel;
+
     private GameObject activeOverlay;
 
     void Start() {
+        panel.color = new Color(0, 0, 0, helpPanelAlpha.x / 255); // (r,g,b) => (0,0,0) == Color.Black
         activeOverlay = mainMenu;
     }
 
     public void Play() {
         activeOverlay.SetActive(false);
         levelSelector.SetActive(true);
+        activeOverlay = levelSelector;
     }
 
     public void Options() {
         activeOverlay.SetActive(false);
         options.SetActive(true);
+        activeOverlay = options;
     }
 
     public void Help() {
         activeOverlay.SetActive(false);
+        panel.color = new Color(0, 0, 0, helpPanelAlpha.y / 255);
         help.SetActive(true);
+        activeOverlay = help;
     }
 
     public void Exit() {
         Application.Quit();
+    }
+
+    public void Back() {
+        activeOverlay.SetActive(false);
+        panel.color = new Color(0, 0, 0, helpPanelAlpha.x / 255);
+        mainMenu.SetActive(true);
+        activeOverlay = mainMenu;
     }
 
 }
