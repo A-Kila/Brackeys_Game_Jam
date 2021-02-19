52f511d1dfe6a7e602e313ede264ded0ca77384c
A-Kila <akila19@freeuni.edu.ge>
2021-02-19 22:12:53 +0400

    update functionality

--- Brackeys Game Jam - 2021 Feb/Assets/Scripts/UIScripts/InGameCanvasManager.cs
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/UIScripts/InGameCanvasManager.cs
@@ -3,6 +3,8 @@ using TMPro;
 
 public class InGameCanvasManager : MonoBehaviour {
 	
+    public GameObject upgradeButton;
+    public GameObject shopMenu;
     public GameObject pause;
     public TextMeshProUGUI text;
 
@@ -16,6 +18,18 @@ public class InGameCanvasManager : MonoBehaviour {
         pauseMng.PauseUpdate();
 
         text.text = GameHandler.money.ToString() + "c";
+    }
+
+    public void Upgrade() {
+        Time.timeScale = 0f;
+        upgradeButton.SetActive(false);
+        shopMenu.SetActive(true);
+    }
+
+    public void Back() {
+        Time.timeScale = 1f;
+        upgradeButton.SetActive(true);
+        shopMenu.SetActive(false);
     }
 
 }
