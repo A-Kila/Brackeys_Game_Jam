b120a113c31e3b47c54eb88adeee3db2c6366ecb
A-Kila <akila19@freeuni.edu.ge>
2021-02-19 16:17:45 +0400

    Main Menu Over

---
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/Levels.cs
@@ -0,0 +1,7 @@
+﻿
+public static class Levels {
+	
+    public static int numLevels = 6;
+    public static bool[] isUnlocked = { true, false, false, false, false, false };
+
+}
---
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/UIScripts/LevelSelectorManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectorManager : MonoBehaviour {
+	
+    private GameObject[] levelButtons;
+    private Vector3 unlockedColor = new Vector3(255f, 255f, 255f); // white
+    private Vector3 lockedColor = new Vector3(150f, 150f, 150f); // grey
+
+
+    void Start() {
+        levelButtons = GameObject.FindGameObjectsWithTag("LevelSelector");
+        Debug.Log(levelButtons.Length);
+        for (int i = 0; i < levelButtons.Length; i++) {
+            Vector3 c;
+
+            if (Levels.isUnlocked[i]) c = unlockedColor;
+            else c = lockedColor;
+        
+            c /= 255;
+            levelButtons[i].GetComponent<Image>().color = new Color(c.x, c.y, c.z);
+        }
+    }
+
+    public void Level1() { GoToLevel(1); }
+    public void Level2() { GoToLevel(2); }
+    public void Level3() { GoToLevel(3); }
+    public void Level4() { GoToLevel(4); }
+    public void Level5() { GoToLevel(5); }
+    public void Level6() { GoToLevel(6); }
+
+    private void GoToLevel(int level) {
+        if (Levels.isUnlocked[level - 1])
+            SceneManager.LoadSceneAsync(level);
+    }
+
+}
