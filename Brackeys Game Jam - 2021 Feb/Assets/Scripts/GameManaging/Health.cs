8b4c2b02e6666f434387827a25349ae95de3399a
Nargizi <77239668+Nargizi@users.noreply.github.com>
2021-02-20 20:54:55 +0400

    Revert "Revert "Healthbar added""
    This reverts commit 824cb9dd75a7175e262e318c9b2b5a269afaa96c.

--- Brackeys Game Jam - 2021 Feb/Assets/Scripts/GameManaging/Health.cs
+++ Brackeys Game Jam - 2021 Feb/Assets/Scripts/GameManaging/Health.cs
@@ -3,18 +3,28 @@
 public class Health {
 	
 	public System.Action onPlayerDeath;
+	public HealthBarControler healthBar;
 
 	private int maxHealth;
 	public int health;
 
-	public Health(int health) {
+	public Health(int health, HealthBarControler healthBar) {
+		this.healthBar = healthBar;
 		maxHealth = health;
 		this.health = maxHealth;
+		healthBar.SetMaxHealth(health);
     }
+
+	public Health(int health)
+    {
+		maxHealth = health;
+		this.health = maxHealth;
+	}
 
 	public void DamageHealth(int amount) {
 		health -= amount;
 		health = Mathf.Clamp(health, 0, maxHealth);
+		if(healthBar != null)healthBar.SetHealth(health);
 
 		if (health <= 0 && onPlayerDeath != null)
 			onPlayerDeath();
@@ -23,6 +33,7 @@ public class Health {
 	public void AddHealth(int amount) {
 		health += amount;
 		health = Mathf.Clamp(health, 0, maxHealth);
-    }
+		if(healthBar != null)healthBar.SetHealth(health);
+	}
 
 }
